97dd075ee96af877659d0d8bb765bd872d84777e
MachineMuse <claire.alexandria@gmail.com>
2013-11-12 23:47:53 -0700

    added particledictionary

---
+++ src/net/machinemuse/numina/render/ParticleDictionary.scala
@@ -0,0 +1,44 @@
+package net.machinemuse.numina.render
+
+/**
+ * Author: MachineMuse (Claire Semple)
+ * Created: 11:39 PM, 11/12/13
+ */
+object ParticleDictionary {
+  val hugeexplosion = "hugeexplosion"
+  val largeexplode = "largeexplode"
+  val fireworksSpark = "fireworksSpark"
+  val bubble = "bubble"
+  val suspended = "suspended"
+  val depthsuspend = "depthsuspend"
+  val townaura = "townaura"
+  val crit = "crit"
+  val magicCrit = "magicCrit"
+  val smoke = "smoke"
+  val mobSpell = "mobSpell"
+  val mobSpellAmbient = "mobSpellAmbient"
+  val spell = "spell"
+  val instantSpell = "instantSpell"
+  val witchMagic = "witchMagic"
+  val note = "note"
+  val enchantmenttable = "enchantmenttable"
+  val explode = "explode"
+  val flame = "flame"
+  val lava = "lava"
+  val footstep = "footstep"
+  val splash = "splash"
+  val largesmoke = "largesmoke"
+  val portal = "portal"
+  val reddust = "reddust"
+  val cloud = "cloud"
+  val snowballpoof = "snowballpoof"
+  val dripWater = "dripWater"
+  val dripLava = "dripLava"
+  val snowshovel = "snowshovel"
+  val slime = "slime"
+  val heart = "heart"
+  val angryVillager = "angryVillager"
+  val happyVillager = "happyVillager"
+  def iconcrack_(itemID:Int) = "iconcrack_" + itemID
+  def tilecrack_(blockID:Int) = "tilecrack_" + blockID
+}
