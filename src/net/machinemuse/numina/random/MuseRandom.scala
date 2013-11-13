6e494777aa2c6aa3d5cf09d385ca383b3e579094
MachineMuse <claire.alexandria@gmail.com>
2013-11-13 03:19:03 -0700

    more work on incense

--- src/net/machinemuse/numina/random/MuseRandom.scala
+++ src/net/machinemuse/numina/random/MuseRandom.scala
@@ -6,8 +6,5 @@ import java.util.Random
  * Author: MachineMuse (Claire Semple)
  * Created: 10:25 PM, 11/12/13
  */
-object MuseRandom {
-  val random = new Random()
-
-  def nextDouble = random.nextDouble()
+object MuseRandom extends Random {
 }
---
+++ src/net/machinemuse/numina/tileentity/MuseTileEntity.scala
@@ -0,0 +1,37 @@
+package net.machinemuse.numina.tileentity
+
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.network.INetworkManager
+import net.minecraft.network.packet.{Packet, Packet132TileEntityData}
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.item.ItemStack
+
+/**
+ * Author: MachineMuse (Claire Semple)
+ * Created: 2:46 AM, 11/13/13
+ */
+class MuseTileEntity extends TileEntity {
+
+  override def onDataPacket(net: INetworkManager, pkt: Packet132TileEntityData) {
+    readFromNBT(pkt.data)
+    worldObj.markBlockForUpdate(xCoord, yCoord, zCoord)
+  }
+
+  override def getDescriptionPacket: Packet = {
+    val tag = new NBTTagCompound
+    writeToNBT(tag)
+    new Packet132TileEntityData(xCoord, yCoord, zCoord, 0, tag)
+  }
+
+  def getInteger(nbt: NBTTagCompound, name: String) = if (nbt.hasKey(name)) Some(nbt.getInteger(name)) else None
+
+  def getDouble(nbt: NBTTagCompound, name: String) = if (nbt.hasKey(name)) Some(nbt.getDouble(name)) else None
+
+  def getItemStack(nbt: NBTTagCompound, name: String) = if (nbt.hasKey(name)) Some(ItemStack.loadItemStackFromNBT(nbt.getCompoundTag(name))) else None
+
+  def writeItemStack(nbt:NBTTagCompound, name:String, stack:ItemStack) {
+    val itemnbt = new NBTTagCompound()
+    stack.writeToNBT(itemnbt)
+    nbt.setCompoundTag("incense", itemnbt)
+  }
+}
