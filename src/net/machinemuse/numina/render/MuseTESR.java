07eb0ee07a3989c433201b1a3ba875db7ba0ef55
MachineMuse <claire.alexandria@gmail.com>
2014-03-13 05:16:58 -0600

    moved MuseTESR to Numina

---
+++ src/net/machinemuse/numina/render/MuseTESR.java
@@ -0,0 +1,14 @@
+package net.machinemuse.numina.render;
+
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * Author: MachineMuse (Claire Semple)
+ * Created: 4:28 PM, 8/3/13
+ */
+public abstract class MuseTESR extends TileEntitySpecialRenderer {
+    public void bindTextureByName(String tex) {
+        this.bindTexture(new ResourceLocation(tex));
+    }
+}
