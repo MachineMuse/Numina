220f7482860d68ff7d2d80808c25e861a7343b8d
Korynkai <matt@qmxtech.com>
2015-01-27 18:48:59 +0000

    Slightly sidetracked... Add OggAudioData exception as this is being reported to OpenEye already... Even though I should\'ve made it clear this is required...

--- src/main/scala/net/machinemuse/numina/basemod/NuminaProxy.scala
+++ src/main/scala/net/machinemuse/numina/basemod/NuminaProxy.scala
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.FMLCommonHandler
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent
 import cpw.mods.fml.common.network.simpleimpl.IMessage
+import cpw.mods.fml.common.Loader;
 import net.machinemuse.numina.general.MuseLogger
 import net.machinemuse.numina.mouse.MouseEventHandler
 import net.machinemuse.numina.network.{MusePacketRecipeUpdate, MusePacket, PacketSender}
@@ -37,6 +38,11 @@ trait NuminaProxy {
 }
 
 class NuminaProxyClient extends NuminaProxy {
+	override def PreInit() = {
+		if (!Loader.isModLoaded["OggAudioData"]) {
+        throw new net.machinemuse.numina.gui.OggAudioDataRequiredDisplayException
+    }
+	}
   override def Init() = {
     MuseLogger.logDebug("Client Proxy Started")
     MinecraftForge.EVENT_BUS.register(MouseEventHandler)
@@ -49,7 +55,7 @@ class NuminaProxyServer extends NuminaProxy
 
 object NuminaPlayerTracker {
   @SubscribeEvent def onPlayerLogin(event: PlayerLoggedInEvent) {
-//    if (!FMLCommonHandler.instance().getMinecraftServerInstance.isSinglePlayer) {
+    if (!FMLCommonHandler.instance().getMinecraftServerInstance.isSinglePlayer) {
       for (recipe <- JSONRecipeList.getJSONRecipesList.toArray) {
         val os: ByteArrayOutputStream = new ByteArrayOutputStream()
         val writer: OutputStreamWriter = new OutputStreamWriter(os)
@@ -59,7 +65,7 @@ object NuminaPlayerTracker {
 
         PacketSender.sendTo(new MusePacketRecipeUpdate(event.player, new ByteArrayInputStream(os.toByteArray)), event.player.asInstanceOf[EntityPlayerMP])
       }
-//    }
+    }
   }
 }
 
---
+++ src/main/scala/net/machinemuse/numina/gui/OggAudioDataRequiredDisplayException.scala
@@ -0,0 +1,22 @@
+package net.machinemuse.numina.gui
+
+import net.minecraft.client.gui.{GuiErrorScreen, FontRenderer}
+import cpw.mods.fml.client.CustomModLoadingErrorDisplayException
+
+class OggAudioDataRequiredDisplayException extends CustomModLoadingErrorDisplayException {
+  override def initGui(errorScreen: GuiErrorScreen, fontRenderer: FontRenderer) {
+  }
+
+  override def drawScreen(errorScreen: GuiErrorScreen, fontRenderer: FontRenderer, mouseRelX: Int, mouseRelY: Int, tickTime: Float) {
+    errorScreen.drawDefaultBackground
+    var offset: Int = 75
+    errorScreen.drawCenteredString(fontRenderer, "A required library is missing. Minecraft cannot continue loading.", errorScreen.width / 2, offset, 0xFFFFFF)
+    offset += 25
+    errorScreen.drawCenteredString(fontRenderer, "The mod: \"Numina\"", errorScreen.width / 2, offset, 0xEEEEEE)
+    offset += 15
+    errorScreen.drawCenteredString(fontRenderer, "requires an additional library: \"OggAudioData\"", errorScreen.width / 2, offset, 0xEEEEEE)
+    offset += 25
+    errorScreen.drawCenteredString(fontRenderer, "Please install \"OggAudioData\" to continue.", errorScreen.width / 2, offset, 0xFFFFFF)
+  }
+
+}
