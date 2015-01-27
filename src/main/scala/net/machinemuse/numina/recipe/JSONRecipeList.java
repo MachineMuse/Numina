b80828f2891c6b1292663306cf8ea908333c5678
Korynkai <matt@qmxtech.com>
2015-01-27 17:21:59 +0000

    Fix typos...

--- src/main/scala/net/machinemuse/numina/recipe/JSONRecipeList.java
+++ src/main/scala/net/machinemuse/numina/recipe/JSONRecipeList.java
@@ -56,7 +56,7 @@ public class JSONRecipeList {
     public static void loadRecipesFromResource(URL resource) {
         try {
             //String json = Resources.toString(resource, Charsets.UTF_8);
-            loadRecipesFromString(new FileInputStream(resource.toString));
+            loadRecipesFromStream(new FileInputStream(resource.toString()));
         } catch(IOException e) {
             e.printStackTrace();
         }
