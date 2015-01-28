8a51f443710152a5d6c9fe867b59e7e1913ac8be
Korynkai <matt@qmxtech.com>
2015-01-28 03:54:20 +0000

    ...

--- src/main/scala/net/machinemuse/numina/basemod/NuminaProxy.scala
+++ src/main/scala/net/machinemuse/numina/basemod/NuminaProxy.scala
@@ -40,7 +40,7 @@ trait NuminaProxy {
 class NuminaProxyClient extends NuminaProxy {
   override def PreInit() = {
     import scala.collection.JavaConversions._
-	if (! (Class.forName("com.qmxtech.oggaudiodata.OggAudioData").getName() == "OggAudioData") ) {
+	if (! (Class.forName("com.qmxtech.oggaudiodata.OggAudioData").getName() == "com.qmxtech.oggaudiodata.OggAudioData") ) {
         throw new net.machinemuse.numina.gui.OggAudioDataRequiredDisplayException
     }
   }
