0741e80bfa4a617b4f97d6ca3f00047dcc0ab098
Korynkai <matt@qmxtech.com>
2015-01-29 18:45:13 +0000

    Add some tests to see if it is getting here...

--- src/main/scala/net/machinemuse/numina/nei/JSONRecipeHandler.java
+++ src/main/scala/net/machinemuse/numina/nei/JSONRecipeHandler.java
@@ -100,9 +100,11 @@ public class JSONRecipeHandler extends ShapedRecipeHandler {
     @Override
     public void loadUsageRecipes(ItemStack ingredient)
     {
+        System.out.println("In loadUsageRecipes")
         List<IRecipe> allrecipes = CraftingManager.getInstance().getRecipeList();
         for(IRecipe irecipe : allrecipes)
         {
+            System.out.println("In loadUsageRecipes loop")
             CachedShapedRecipe recipe = null;
             if(irecipe instanceof JSONRecipe)
                 recipe = JSONShapedRecipe((JSONRecipe) irecipe);
@@ -113,6 +115,7 @@ public class JSONRecipeHandler extends ShapedRecipeHandler {
             recipe.computeVisuals();
             if(recipe.contains(recipe.ingredients, ingredient))
             {
+                System.out.println("Pushing to NEI")
                 recipe.setIngredientPermutation(recipe.ingredients, ingredient);
                 arecipes.add(recipe);
             }
