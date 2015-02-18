f79212972e8e927fdb88c31e062b62c2356e3af9
Korynkai <matt@qmxtech.com>
2015-02-18 13:45:02 +0000

    Don\'t know how this happened...

--- src/main/scala/net/machinemuse/numina/basemod/Numina.scala
+++ src/main/scala/net/machinemuse/numina/basemod/Numina.scala
@@ -44,7 +44,7 @@ object Numina {
     proxy.PostInit()
   }
   
-  @Mod.EventHandler def servertsrtaing(e: FMLSererSvtartingEvent) {
+  @Mod.EventHandler def serverstarting(e: FMLServerStartingEvent) {
     JSONRecipeList.loadRecipesFromDir(configDir.getAbsolutePath + "/machinemuse/recipes")
   }
 
