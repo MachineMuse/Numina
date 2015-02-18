d7ffa33184165c9400133c24285f478c6e7a6c6f
Korynkai <matt@qmxtech.com>
2015-02-18 18:07:50 +0000

    Fix access modifier in JSONRecipeHandler

--- src/main/scala/net/machinemuse/numina/recipe/JSONRecipeList.java
+++ src/main/scala/net/machinemuse/numina/recipe/JSONRecipeList.java
@@ -25,7 +25,7 @@ import java.util.List;
  * TODO: Refactor to also handle Shapeless and Smelting
  */
 public class JSONRecipeList {
-    static List<JSONRecipe> recipesList = new ArrayList<JSONRecipe>();
+    public static List<JSONRecipe> recipesList = new ArrayList<JSONRecipe>();
     public static Gson gson = new GsonBuilder().registerTypeAdapterFactory(new EnumTypeAdapterFactory()).setPrettyPrinting().create();
 
     private static FilenameFilter filter = new FilenameFilter() {
