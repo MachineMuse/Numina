3f95b0d8de2468a338543866935f64d630784983
MachineMuse <muse@machinemuse.net>
2015-08-27 00:52:51 -0600

    added soft nbt support back in for recipe outputs (won't break on obfuscation), also added gameregistry lookups

--- src/main/scala/net/machinemuse/numina/recipe/SimpleItemMaker.java
+++ src/main/scala/net/machinemuse/numina/recipe/SimpleItemMaker.java
@@ -1,9 +1,13 @@
 package net.machinemuse.numina.recipe;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.machinemuse.numina.general.MuseLogger;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTException;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -16,7 +20,9 @@ public class SimpleItemMaker implements IItemMaker {
     public Integer quantity;
     public String unlocalizedName;
     public String oredictName;
-//    public NBTTagCompound nbt;
+    public String registryName;
+    public String itemStackName;
+    public String nbtString;
 
     @Override
     public ItemStack makeItem(InventoryCrafting i) {
@@ -34,10 +40,42 @@ public class SimpleItemMaker implements IItemMaker {
     public ItemStack getRecipeOutput() {
         int newmeta = getOrElse(this.meta, 0);
         int newquantity = getOrElse(this.quantity, 1);
-        if (oredictName != null) {
+        NBTTagCompound nbt = null;
+
+        if(nbtString != null) {
+            try {
+                nbt = (NBTTagCompound) JsonToNBT.func_150315_a(nbtString);
+            } catch (Exception e) {
+                MuseLogger.logException("Bad NBT string in item! Attempting to create generic item instead.", e);
+            }
+        }
+
+        if (itemStackName != null) {
+            try {
+                String[] names = registryName.split(":");
+                ItemStack stack = GameRegistry.findItemStack(names[0], names[1], newquantity);
+                if(this.meta != null) stack.setItemDamage(meta);
+                if(nbt != null) stack.setTagCompound(nbt);
+                return stack;
+            } catch (Exception e) {
+                MuseLogger.logError("Unable to load " + registryName + " from Item Registry");
+                return null;
+            }
+        } else if (registryName != null) {
+            try {
+                String[] names = registryName.split(":");
+                ItemStack stack = new ItemStack(GameRegistry.findItem(names[0], names[1]), newquantity, newmeta);
+                if(nbt != null) stack.setTagCompound(nbt);
+                return stack;
+            } catch (Exception e) {
+                MuseLogger.logError("Unable to load " + registryName + " from Item Registry");
+                return null;
+            }
+        } else if (oredictName != null) {
             try {
                 ItemStack stack = OreDictionary.getOres(oredictName).get(0).copy();
-                stack.stackSize = newquantity;
+                stack.stackSize = Math.min(newquantity, stack.getMaxStackSize());
+                if(nbt != null) stack.setTagCompound(nbt);
                 return stack;
             } catch (Exception e) {
                 MuseLogger.logError("Unable to load " + oredictName + " from oredict");
@@ -48,7 +86,8 @@ public class SimpleItemMaker implements IItemMaker {
                 ItemStack stack = ItemNameMappings.getItem(unlocalizedName).copy();
                 newmeta = getOrElse(this.meta, stack.getItemDamage());
                 stack.setItemDamage(newmeta);
-                stack.stackSize = newquantity;
+                stack.stackSize = Math.min(newquantity, stack.getMaxStackSize());
+                if(nbt != null) stack.setTagCompound(nbt);
                 return stack;
             } catch (Exception e) {
                 MuseLogger.logError("Unable to load " + unlocalizedName + " from unlocalized names");
