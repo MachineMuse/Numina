ddde32f3aca5399ff614567e2bab322afe36733e
Leon <lehjr1@gmail.com>
2016-08-27 11:23:45 -0400

    Fix TileEntity not saving on exit. Updated to latest Forge.

--- src/main/scala/net/machinemuse/numina/tileentity/MuseTileEntity.scala
+++ src/main/scala/net/machinemuse/numina/tileentity/MuseTileEntity.scala
@@ -1,38 +1,33 @@
 package net.machinemuse.numina.tileentity
 
+import net.machinemuse.numina.general.MuseLogger
+import net.minecraft.block.state.IBlockState
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.network.NetworkManager
 import net.minecraft.network.play.server.SPacketUpdateTileEntity
 import net.minecraft.tileentity.TileEntity
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import net.minecraftforge.fml.common.FMLCommonHandler
 
 /**
  * Author: MachineMuse (Claire Semple)
  * Created: 2:46 AM, 11/13/13
  */
 class MuseTileEntity extends TileEntity {
-  // TODO: test and clean up
-  // getDescriptionPacket is no longer used available
-
   override def onDataPacket(net: NetworkManager, pkt: SPacketUpdateTileEntity) {
-    readFromNBT(pkt.getNbtCompound())
-
-    // 1.7.10
-//    worldObj.markBlockForUpdate(xCoord, yCoord, zCoord)
-    // 1.8.9
-//    worldObj.markBlockForUpdate(pos)
-
-    // 1.9.4 ?? TODO: does this still need markBlockForUpdate??
+    readFromNBT(pkt.getNbtCompound)
+    val state = getWorld.getBlockState(getPos)
+    getWorld.notifyBlockUpdate(getPos, state, state, 3)
   }
 
-  override def getUpdatePacket: SPacketUpdateTileEntity = {
-    val tag = new NBTTagCompound
-    writeToNBT(tag)
-    new SPacketUpdateTileEntity(getPos, 0, tag)
+  override def getUpdatePacket(): SPacketUpdateTileEntity = {
+    new SPacketUpdateTileEntity(getPos, getBlockMetadata, getUpdateTag)
   }
 
-  override def getUpdateTag : NBTTagCompound = writeToNBT(new NBTTagCompound())
-
+  override def getUpdateTag(): NBTTagCompound = writeToNBT(new NBTTagCompound())
 
   def getInteger(nbt: NBTTagCompound, name: String) = if (nbt.hasKey(name)) Some(nbt.getInteger(name)) else None
 
@@ -47,4 +42,10 @@ class MuseTileEntity extends TileEntity {
     stack.writeToNBT(itemnbt)
     nbt.setTag(name, itemnbt)
   }
+
+  override def shouldRefresh(world: World,
+                             pos: BlockPos,
+                             oldState: IBlockState,
+                             newSate: IBlockState): Boolean = oldState.getBlock != newSate.getBlock
+
 }
