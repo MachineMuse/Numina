5f4ddb302d8d8773860e3c5048d0164b20361325
lehjr <lehjr1@gmail.com>
2019-02-27 09:16:02 -0500

    Revert "Enable FOV change when sprinting while still limiting extreme values"

--- src/main/java/net/machinemuse/numina/event/FOVUpdateEventHandler.java
+++ src/main/java/net/machinemuse/numina/event/FOVUpdateEventHandler.java
@@ -9,22 +9,17 @@ import net.minecraftforge.client.event.FOVUpdateEvent;
 /**
  * Author: MachineMuse (Claire Semple)
  * Created: 10:07 PM, 10/17/13
- * <p>
+ *
  * Ported to Java by lehjr on 10/10/16.
  */
 public class FOVUpdateEventHandler {
-
-	@SubscribeEvent
-	public void onFOVUpdate(FOVUpdateEvent e) {
-		if (NuminaConfig.useFOVFix()) {
-			IAttributeInstance attributeinstance = e.entity.getEntityAttribute(SharedMonsterAttributes.movementSpeed);
-			e.newfov = (float) (e.fov / ((attributeinstance.getAttributeValue() / e.entity.capabilities.getWalkSpeed() + 1.0) / 2.0));
-			if (e.entity.isSprinting()) {
-				e.newfov += 0.15F;
-			}
-		}
-	}
-
+    @SubscribeEvent
+    public void onFOVUpdate(FOVUpdateEvent e) {
+        if (NuminaConfig.useFOVFix()) {
+            IAttributeInstance attributeinstance = e.entity.getEntityAttribute(SharedMonsterAttributes.movementSpeed);
+            e.newfov = (float) (e.fov / ((attributeinstance.getAttributeValue() / e.entity.capabilities.getWalkSpeed() + 1.0) / 2.0));
+        }
+    }
 }
 
 
